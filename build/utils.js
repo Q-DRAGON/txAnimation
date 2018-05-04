11f98f3be426fc4117b3de38d376d8934efa0e1a
Christina <z574203238@outlook.com>
2018-05-05 03:13:05 +0800

    add

--- build/utils.js
+++ build/utils.js
@@ -48,7 +48,7 @@ exports.cssLoaders = function (options) {
       return ExtractTextPlugin.extract({
         use: loaders,
         fallback: 'vue-style-loader',
-        publicPath: '../../'
+        publicPath: '/txAnimation/dist/'
       })
     } else {
       return ['vue-style-loader'].concat(loaders)
